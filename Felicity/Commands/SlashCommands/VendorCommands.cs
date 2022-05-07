077818e013d27e09baabc7b7e9d64ccd01edacdd
Willow <will09600@gmail.com>
2022-05-07 20:06:05 +0200

    Add /vendor saint14

--- Felicity/Commands/SlashCommands/VendorCommands.cs
+++ Felicity/Commands/SlashCommands/VendorCommands.cs
@@ -1,7 +1,13 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using APIHelper;
+using BungieSharper.Entities.Destiny;
+using Discord;
 using Discord.Interactions;
+using Felicity.Enums;
 using Felicity.Helpers;
+using Felicity.Services;
 using Felicity.Structs;
 
 // ReSharper disable UnusedMember.Global
@@ -9,10 +15,10 @@ using Felicity.Structs;
 
 namespace Felicity.Commands.SlashCommands;
 
+[RequireOAuth]
 [Group("vendor", "Group of commands related to vendors and their available items.")]
 public class VendorCommands : InteractionModuleBase<SocketInteractionContext>
 {
-    [RequireOAuth]
     [SlashCommand("xur", "Fetch Xûr inventory which includes D2Gunsmith and LightGG links.")]
     public async Task Xur()
     {
@@ -29,14 +35,101 @@ public class VendorCommands : InteractionModuleBase<SocketInteractionContext>
         }
         else
         {
-            if(File.Exists("Data/xurCache.json"))
+            if (File.Exists("Data/xurCache.json"))
                 File.Delete("Data/xurCache.json");
 
             await FollowupAsync(embed: ProcessXurData.BuildUnavailableEmbed());
         }
     }
 
-    [RequireOAuth]
+    [SlashCommand("saint14", "Fetch Saint-14 (Trials of Osiris) reputation rewards for the week.")]
+    public async Task Saint()
+    {
+        await DeferAsync();
+
+        var oauth = Context.User.OAuth();
+        var destinyMembership = oauth.DestinyMembership;
+
+        var vendorData = APIService.GetApiClient().Api.Destiny2_GetVendor(destinyMembership.CharacterIds.First(),
+                destinyMembership.MembershipId,
+                destinyMembership.MembershipType, (uint) VendorIds.Saint14, new[]
+                {
+                    DestinyComponentType.ItemPerks, DestinyComponentType.ItemSockets,
+                    DestinyComponentType.Vendors, DestinyComponentType.VendorCategories,
+                    DestinyComponentType.VendorSales
+                },
+                oauth.AccessToken)
+            .Result;
+
+        if (vendorData.Vendor.Data.Progression.CurrentResetCount >= 3)
+        {
+            await FollowupAsync(embed: Extensions.GenerateVendorEmbed("Saint-14", Images.SaintVendorLogo,
+                    $"Because you've reset your rank {Format.Bold(vendorData.Vendor.Data.Progression.CurrentResetCount.ToString())} times, " +
+                    "Saint-14 no longer sells weapons with fixed rolls for you.")
+                .Build(), ephemeral: true);
+            return;
+        }
+
+        var repRewards = vendorData.Categories.Data.Categories.ElementAt(3).ItemIndexes;
+
+        var embed = Extensions.GenerateVendorEmbed("Saint-14", Images.SaintVendorLogo,
+            "A legendary hero and the former Titan Vanguard, Saint-14 now manages the PvP gamemode Trials of Osiris.\n\n" +
+            "These rewards change perks based on weekly reset.");
+
+        var i = 0;
+
+        foreach (var repReward in repRewards)
+        {
+            var reward = vendorData.Sales.Data[repReward];
+
+            if (reward.Quantity != 1)
+                continue;
+
+            var manifestItem = ManifestConnection.GetInventoryItemById(unchecked((int) reward.ItemHash));
+
+            if (manifestItem.ItemType != DestinyItemType.Weapon)
+                continue;
+
+            var name = manifestItem.DisplayProperties.Name;
+
+            var fullMessage = $"[{name}]({WeaponHelper.BuildLightGGLink(manifestItem.Hash)})\n";
+
+            var plugHash1 = vendorData.ItemComponents.Sockets.Data[repReward].Sockets.ElementAt(3).PlugHash;
+            if (plugHash1 != null)
+            {
+                var perk1 = ManifestConnection.GetInventoryItemById(unchecked((int) plugHash1));
+                fullMessage += EmoteHelper.GetEmote(perk1.DisplayProperties.Icon, perk1.DisplayProperties.Name);
+            }
+
+            var plugHash2 = vendorData.ItemComponents.Sockets.Data[repReward].Sockets.ElementAt(4).PlugHash;
+            if (plugHash2 != null)
+            {
+                var perk2 = ManifestConnection.GetInventoryItemById(unchecked((int) plugHash2));
+                fullMessage += EmoteHelper.GetEmote(perk2.DisplayProperties.Icon, perk2.DisplayProperties.Name);
+            }
+
+            var fieldName = i == 0 ? "Rank 10" : "Rank 16";
+
+            embed.AddField(new EmbedFieldBuilder
+            {
+                IsInline = true,
+                Name = fieldName,
+                Value = fullMessage
+            });
+
+            i++;
+        }
+
+        embed.AddField(new EmbedFieldBuilder
+        {
+            IsInline = true,
+            Name = "Current Rank",
+            Value = vendorData.Vendor.Data.Progression.Level
+        });
+
+        await FollowupAsync(embed: embed.Build());
+    }
+
     [SlashCommand("mods", "Get list of mods currently available at vendors.")]
     public async Task Mods()
     {
--- Felicity/Enums/Strings.cs
+++ Felicity/Enums/Strings.cs
@@ -25,4 +25,6 @@ internal static class Images
     public const string ModVendorIcon = "https://bungie.net/common/destiny2_content/icons/23599621d4c63076c647384028d96ca4.png";
 
     public const string XurVendorLogo = "https://www.bungie.net/img/destiny_content/vendor/icons/xur_large_icon.png";
+
+    public const string SaintVendorLogo = "https://bungie.net/common/destiny2_content/icons/c3cb40c2b36cccd2f6cf462f14c89736.png";
 }
