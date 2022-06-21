349f12470411bdf2f67865839765decb2d7393e8
MeGl <neloangelo3105@gmail.com>
2022-06-21 15:57:09 +0300

    Added ping command example

---
+++ Felicity/DiscordCommands/Interactions/PingCommandExample.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace Felicity.DiscordCommands.Interactions;
+
+public class PingCommandExample : InteractionModuleBase<ShardedInteractionContext<SocketSlashCommand>>
+{
+    public PingCommandExample()
+    {
+    }
+
+    [SlashCommand("ping", "Pongs back")]
+    public async Task PongAsync(
+        [Summary("text to pong back")] string? text = null)
+    {
+        var eb = new EmbedBuilder()
+            .WithTitle("Glorious embed title")
+            .WithDescription("Description? Idk what you expected in ping command")
+            .AddField("PONG", "PONG");
+        
+        if (text is not null && text != "")
+        {
+            eb.AddField("Some custom text", text);
+        }
+
+        await Context.Interaction.RespondAsync(embed: eb.Build(), ephemeral: true);
+    }
+}
