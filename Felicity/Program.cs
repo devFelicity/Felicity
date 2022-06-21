00bfa3419a5ccaeea2dbc56fd2bcdd66f1a11de2
MeGl <neloangelo3105@gmail.com>
2022-06-21 14:50:30 +0300

    Added serilog setup

--- Felicity/Program.cs
+++ Felicity/Program.cs
@@ -2,45 +2,74 @@
 using DotNetBungieAPI.Models;
 using DotNetBungieAPI.Models.Applications;
 using Felicity.Extensions;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services
-    .AddDiscord(
-        discordClient =>
-        {
-            // configure your client here
-        },
-        interactionService =>
-        {
-            // configure your interaction service here
-        },
-        textCommandsService =>
-        {
-            // configure your text commands service here
-        },
-        builder.Configuration)
-    .UseBungieApiClient(bungieClient =>
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Quartz", LogEventLevel.Information)
+    .WriteTo.Console()
+    .WriteTo.File("Logs/latest-.log", rollingInterval: RollingInterval.Day)
+    .WriteTo.Sentry(o =>
     {
-        bungieClient.ApiKey = "ApiKey";
-        bungieClient.ApplicationScopes = ApplicationScopes.ReadBasicUserProfile;
-        bungieClient.CacheDefinitions = true;
-        bungieClient.ClientId = 123;
-        bungieClient.ClientSecret = "secret_here";
-        bungieClient.UsedLocales.Add(BungieLocales.EN);
-        bungieClient
-            .UseDefaultDefinitionProvider(definitionProvider =>
-            {
-                definitionProvider.ManifestFolderPath = "path where all manifests would be stored";
-                definitionProvider.AutoUpdateManifestOnStartup = true;
-                definitionProvider.FetchLatestManifestOnInitialize = true;
-                definitionProvider.DeleteOldManifestDataAfterUpdates = false;
-            })
-            .UseDefaultHttpClient(httpClient =>
-            {
-                httpClient.SetRatelimitSettings(200, TimeSpan.FromSeconds(10));
-            });
-    });
+        o.AttachStacktrace = true;
+        o.Dsn = "Get sentry Dsn from config here";
+        o.MinimumBreadcrumbLevel = LogEventLevel.Debug;
+        o.MinimumEventLevel = LogEventLevel.Warning;
+        o.Release = $"FelicityOne@{"Get version here"}";
+    })
+    .CreateLogger();
 
-var app = builder.Build();
-await app.RunAsync();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services
+        .AddDiscord(
+            discordClient =>
+            {
+                // configure your client here
+            },
+            interactionService =>
+            {
+                // configure your interaction service here
+            },
+            textCommandsService =>
+            {
+                // configure your text commands service here
+            },
+            builder.Configuration)
+        .UseBungieApiClient(bungieClient =>
+        {
+            bungieClient.ApiKey = "ApiKey";
+            bungieClient.ApplicationScopes = ApplicationScopes.ReadBasicUserProfile;
+            bungieClient.CacheDefinitions = true;
+            bungieClient.ClientId = 123;
+            bungieClient.ClientSecret = "secret_here";
+            bungieClient.UsedLocales.Add(BungieLocales.EN);
+            bungieClient
+                .UseDefaultDefinitionProvider(definitionProvider =>
+                {
+                    definitionProvider.ManifestFolderPath = "path where all manifests would be stored";
+                    definitionProvider.AutoUpdateManifestOnStartup = true;
+                    definitionProvider.FetchLatestManifestOnInitialize = true;
+                    definitionProvider.DeleteOldManifestDataAfterUpdates = false;
+                })
+                .UseDefaultHttpClient(httpClient =>
+                {
+                    httpClient.SetRatelimitSettings(200, TimeSpan.FromSeconds(10));
+                });
+        });
+
+    var app = builder.Build();
+    await app.RunAsync();
+}
+catch (Exception exception)
+{
+    Log.Fatal(exception, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
