f412c7e8cd47c0a2e0bedf760df3a5730bc5e9a7
Willow <will09600@gmail.com>
2022-06-23 15:30:49 +0200

    Add LogFormat for discord

--- Felicity/Options/DiscordBotOptions.cs
+++ Felicity/Options/DiscordBotOptions.cs
@@ -1,7 +1,10 @@
-﻿namespace Felicity.Options;
+﻿using Discord;
+
+namespace Felicity.Options;
 
 public class DiscordBotOptions
 {
     public string? Token { get; set; }
     public string? Prefix { get; set; }
+    public Func<LogMessage, Exception?, string> LogFormat { get; set; } = (message, _) => $"{message.Source}: {message.Message}";
 }
