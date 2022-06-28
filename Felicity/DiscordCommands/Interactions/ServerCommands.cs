412f2734fb5027087c815c23fcf924f739240a7e
Willow <will09600@gmail.com>
2022-06-28 18:38:06 +0200

    Add /server summary

--- Felicity/DiscordCommands/Interactions/ServerCommands.cs
+++ Felicity/DiscordCommands/Interactions/ServerCommands.cs
@@ -1,9 +1,9 @@
 ﻿using Discord;
 using Discord.Interactions;
+using Discord.WebSocket;
 using DotNetBungieAPI.Models;
 using Felicity.Models;
 using Felicity.Services;
-using Felicity.Services.Hosted;
 using Felicity.Util;
 
 // ReSharper disable StringLiteralTypo
@@ -18,7 +18,7 @@ namespace Felicity.DiscordCommands.Interactions;
 public class ServerCommands : InteractionModuleBase<ShardedInteractionContext>
 {
     private readonly ServerDb _serverDb;
-    
+
     public ServerCommands(ServerDb serverDb)
     {
         _serverDb = serverDb;
@@ -26,19 +26,24 @@ public class ServerCommands : InteractionModuleBase<ShardedInteractionContext>
 
     [SlashCommand("configure", "Set up your server.")]
     public async Task ServerConfigure(
-        [Summary("announcements", "Which channel should Felicity send announcements related to bot services?")] ITextChannel announcementChannel,
-        [Summary("staff", "Which channel should Felicity send STAFF-ONLY announcements to?")] ITextChannel staffChannel,
-        [Summary("memberchannel", "Which channel do I send to?")] ITextChannel? memberLogChannel = null,
-        [Summary("memberjoined", "Should I send messages when a member joins?")] bool memberJoined = false,
-        [Summary("memberleft", "Should I send messages when a member leaves?")] bool memberLeft = false)
+        [Summary("announcements", "Which channel should Felicity send announcements related to bot services?")]
+        ITextChannel announcementChannel,
+        [Summary("staff", "Which channel should Felicity send STAFF-ONLY announcements to?")]
+        ITextChannel staffChannel,
+        [Summary("memberchannel", "Which channel do I send to?")]
+        ITextChannel? memberLogChannel = null,
+        [Summary("memberjoined", "Should I send messages when a member joins?")]
+        bool memberJoined = false,
+        [Summary("memberleft", "Should I send messages when a member leaves?")]
+        bool memberLeft = false)
     {
         await DeferAsync(true);
 
         var server = GetServer(Context.Guild.Id);
         server.AnnouncementChannel = announcementChannel.Id;
         server.StaffChannel = staffChannel.Id;
-        
-        if(memberLogChannel != null)
+
+        if (memberLogChannel != null)
             server.MemberLogChannel = memberLogChannel.Id;
 
         server.MemberJoined = memberJoined;
@@ -63,6 +68,54 @@ public class ServerCommands : InteractionModuleBase<ShardedInteractionContext>
         embed.AddField("Member Events", $"Joins: {memberJoined}\nLeaves: {memberLeft}", true);
 
         await FollowupAsync(embed: embed.Build());
+    }
+
+    [SlashCommand("summary", "Get a summary of current server settings.")]
+    public async Task ServerSummary()
+    {
+        await DeferAsync(true);
+
+        var server = GetServer(Context.Guild.Id);
+
+        var embed = Embeds.MakeBuilder();
+        embed.Author = new EmbedAuthorBuilder
+        {
+            Name = Context.Guild.Name,
+            IconUrl = Context.Guild.IconUrl
+        };
+        embed.Description = "Summary of server settings:";
+
+        var channelSummary = $"Announcements: {GetChannel(Context.Guild, server.AnnouncementChannel)}\n" +
+                             $"Staff: {GetChannel(Context.Guild, server.StaffChannel)}";
+        if (server.MemberLogChannel != null)
+            channelSummary += $"\nJoin/Leave: {GetChannel(Context.Guild, server.MemberLogChannel)}";
+
+        embed.AddField("Channels", channelSummary, true);
+        embed.AddField("Language", server.BungieLocale, true);
+        embed.AddField("Member Events", $"Joins: {server.MemberJoined}\nLeaves: {server.MemberLeft}", true);
+
+        await FollowupAsync(embed: embed.Build());
+    }
+
+    private static string GetChannel(SocketGuild guild, ulong? channelId)
+    {
+        return channelId == null ? "not set." : guild.GetTextChannel((ulong)channelId).Mention;
+    }
+
+    private Server GetServer(ulong guildId)
+    {
+        var server = _serverDb.Servers.FirstOrDefault(x => x.ServerId == Context.Guild.Id);
+        if (server != null)
+            return server;
+
+        server = new Server
+        {
+            ServerId = guildId,
+            BungieLocale = BungieLocales.EN
+        };
+        _serverDb.Servers.Add(server);
+
+        return server;
     }
 
     [RequireContext(ContextType.Guild)]
@@ -133,23 +186,8 @@ public class ServerCommands : InteractionModuleBase<ShardedInteractionContext>
 
             _twitchClientService.RestartMonitor();
 
-            await FollowupAsync($"Successfully removed {Format.Bold(stream.TwitchName)}'s stream from server.", ephemeral: true);
+            await FollowupAsync($"Successfully removed {Format.Bold(stream.TwitchName)}'s stream from server.",
+                ephemeral: true);
         }
-    }
-
-    private Server GetServer(ulong guildId)
-    {
-        var server = _serverDb.Servers.FirstOrDefault(x => x.ServerId == Context.Guild.Id);
-        if (server != null)
-            return server;
-
-        server = new Server
-        {
-            ServerId = guildId,
-            BungieLocale = BungieLocales.EN
-        };
-        _serverDb.Servers.Add(server);
-
-        return server;
     }
 }
