082ee4de4e1276eadc7188fd1a4f7919f3fccd79
Willow <will09600@gmail.com>
2022-06-29 15:00:13 +0200

    Migrate fun commands to bytes

--- Felicity/DiscordCommands/Interactions/FunCommands.cs
+++ Felicity/DiscordCommands/Interactions/FunCommands.cs
@@ -2,6 +2,7 @@
 using Discord.Interactions;
 using Felicity.Util;
 
+// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable StringLiteralTypo
 // ReSharper disable UnusedMember.Global
 // ReSharper disable UnusedType.Global
@@ -10,41 +11,54 @@ namespace Felicity.DiscordCommands.Interactions;
 
 public class FunCommands : InteractionModuleBase<ShardedInteractionContext>
 {
-    [SlashCommand("fristy", "Have you ever heard of the cute FristyFox?")]
-    public async Task FunFristy()
+    public static readonly List<string> AvailableBytes = new()
+    {
+        "Fristy is the cutest fox",
+        "Learn about Jess"
+    };
+
+    [SlashCommand("byte", "Miscellaneous commands that don't really need their own command.")]
+    public async Task RunByte(
+        [Autocomplete(typeof(RunByteAutocomplete))] [Summary("byte", "Name of the command to run.")] int byteNumber)
     {
         await DeferAsync();
 
         var embed = Embeds.MakeBuilder();
-        embed.Author = new EmbedAuthorBuilder
-        {
-            IconUrl = "https://cdn.discordapp.com/avatars/139428768669237248/0fe07c14088b6c5f222283f44f34b09b.png",
-            Name = "<= this is FristyFox",
-            Url = "https://twitch.tv/fristyfox"
-        };
-        embed.Description = "FristyFox of the House Mittens, the first of His Name, King of the Pyramids, " +
-                            "and the first Foxes, Lord of the seven discords and the Protector of the Chat, " +
-                            "Lord of Foxstone, King of Snaccville, Khal of the Great Hangout Sea, the Unburnt, " +
-                            "Breaker of Bongobutt Chains, and Father of Cuties <:mittensAWW:594996439508320297> Cutest there is, " +
-                            "Cutest there was, and the Cutest there ever will be!!! NO TAKE BACKS! <:mittensAWW:594996439508320297>";
+        embed = PopulateRunByte(AvailableBytes[byteNumber], embed);
 
         await FollowupAsync(embed: embed.Build());
     }
 
-    [SlashCommand("jess", "Have you ever heard of the cute Jess?")]
-    public async Task FunJess()
+    private static EmbedBuilder PopulateRunByte(string runByte, EmbedBuilder embed)
     {
-        await DeferAsync();
-
-        var embed = Embeds.MakeBuilder();
-        embed.Author = new EmbedAuthorBuilder
+        switch (runByte)
         {
-            IconUrl = "https://cdn.discordapp.com/avatars/894420816542974024/f8a8b6f2cc0619fb01962c9ea225e6ac.png",
-            Name = "<= this is Jess",
-            Url = "https://twitch.tv/tattmeupjess"
-        };
-        embed.Description = "better than a blueberry, is an apple <a:catapplerun:968303617402630184>";
+            case "fristy":
+                embed.Author = new EmbedAuthorBuilder
+                {
+                    IconUrl =
+                        "https://cdn.discordapp.com/avatars/139428768669237248/0fe07c14088b6c5f222283f44f34b09b.png",
+                    Name = "<= this is FristyFox",
+                    Url = "https://twitch.tv/fristyfox"
+                };
+                embed.Description = "FristyFox of the House Mittens, the first of His Name, King of the Pyramids, " +
+                                    "and the first Foxes, Lord of the seven discords and the Protector of the Chat, " +
+                                    "Lord of Foxstone, King of Snaccville, Khal of the Great Hangout Sea, the Unburnt, " +
+                                    "Breaker of Bongobutt Chains, and Father of Cuties <:mittensAWW:594996439508320297> Cutest there is, " +
+                                    "Cutest there was, and the Cutest there ever will be!!! NO TAKE BACKS! <:mittensAWW:594996439508320297>";
+                break;
+            case "jess":
+                embed.Author = new EmbedAuthorBuilder
+                {
+                    IconUrl =
+                        "https://cdn.discordapp.com/avatars/894420816542974024/f8a8b6f2cc0619fb01962c9ea225e6ac.png",
+                    Name = "<= this is Jess",
+                    Url = "https://twitch.tv/tattmeupjess"
+                };
+                embed.Description = "better than a blueberry, is an apple <a:catapplerun:968303617402630184>";
+                break;
+        }
 
-        await FollowupAsync(embed: embed.Build());
+        return embed;
     }
 }
--- Felicity/Util/AutoCompletes.cs
+++ Felicity/Util/AutoCompletes.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.Interactions;
+using Felicity.DiscordCommands.Interactions;
 using Felicity.Models;
 
 namespace Felicity.Util;
@@ -29,6 +30,29 @@ public class TwitchStreamAutocomplete : AutocompleteHandler
             Name = $"{twitchStream.TwitchName} ({context.Guild.GetChannelAsync(twitchStream.ChannelId).Result.Name})",
             Value = twitchStream.Id
         }).ToList();
+
+        return AutocompletionResult.FromSuccess(resultList);
+    }
+}
+
+public class RunByteAutocomplete : AutocompleteHandler
+{
+    public override async Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context,
+        IAutocompleteInteraction autocompleteInteraction,
+        IParameterInfo parameter, IServiceProvider services)
+    {
+        await Task.Delay(0);
+
+        var resultList = new List<AutocompleteResult>();
+        var i = 0;
+
+        var currentSearch = autocompleteInteraction.Data.Current.Value.ToString();
+
+        foreach (var availableByte in FunCommands.AvailableBytes.Where(availableByte => currentSearch != null && availableByte.ToLower().Contains(currentSearch.ToLower())))
+        {
+            resultList.Add(new AutocompleteResult{Name = availableByte, Value = i});
+            i++;
+        }
 
         return AutocompletionResult.FromSuccess(resultList);
     }
