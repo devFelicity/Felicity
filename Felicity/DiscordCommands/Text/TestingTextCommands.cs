3ad613072519b8a725c86c1976a9267eabb03f4a
Willow <will09600@gmail.com>
2022-07-06 13:54:32 +0200

    Add metrics command

--- Felicity/DiscordCommands/Text/TestingTextCommands.cs
+++ Felicity/DiscordCommands/Text/TestingTextCommands.cs
@@ -1,5 +1,11 @@
-﻿using Discord.Commands;
+﻿using System.Diagnostics;
+using Discord;
+using Discord.Commands;
+using DotNetBungieAPI.Clients;
+using Felicity.Models;
 using Felicity.Models.Caches;
+using Felicity.Util;
+using Humanizer;
 
 // ReSharper disable UnusedType.Global
 // ReSharper disable UnusedMember.Global
@@ -8,11 +14,22 @@ namespace Felicity.DiscordCommands.Text;
 
 public class BasicTextCommands : ModuleBase<ShardedCommandContext>
 {
+    private readonly TwitchStreamDb _twitchStreamDb;
+    private readonly UserDb _userDb;
+    private readonly IBungieClient _bungieClient;
+
+    public BasicTextCommands(TwitchStreamDb twitchStreamDb, UserDb userDb, IBungieClient bungieClient)
+    {
+        _twitchStreamDb = twitchStreamDb;
+        _userDb = userDb;
+        _bungieClient = bungieClient;
+    }
+
     [Command("ping")]
     public async Task Pong()
     {
         // ReSharper disable once StringLiteralTypo
-        await ReplyAsync("<:NOOOOOOOOOOOOOT:855149582177533983>");
+        await Context.Message.ReplyAsync("<:NOOOOOOOOOOOOOT:855149582177533983>");
     }
 
     [Command("fillCPs")]
@@ -20,5 +37,44 @@ public class BasicTextCommands : ModuleBase<ShardedCommandContext>
     {
         var msg = await Context.Channel.GetMessageAsync(messageId);
         ProcessCpData.Populate(msg);
+    }
+
+    [Command("metrics")]
+    public async Task Metrics()
+    {
+        var serverList = Context.Client.Guilds;
+        await Context.Client.DownloadUsersAsync(serverList);
+        var userList = new List<ulong>();
+
+        foreach (var clientGuild in serverList)
+        {
+            foreach (var clientGuildUser in clientGuild.Users)
+            {
+                if (clientGuildUser.IsBot)
+                    continue;
+
+                if (!userList.Contains(clientGuildUser.Id))
+                    userList.Add(clientGuildUser.Id);
+            }
+        }
+
+        var manifest = await _bungieClient.DefinitionProvider.GetCurrentManifest();
+        var uptime = DateTime.Now - Process.GetCurrentProcess().StartTime;
+
+        var embed = Embeds.MakeBuilder();
+        embed.Title = "Felicity Metrics";
+        embed.Description = "Overview of bot metrics.";
+        embed.Color = Color.Teal;
+        embed.ThumbnailUrl = "https://icons.iconarchive.com/icons/graphicloads/100-flat/256/analytics-icon.png";
+
+        embed.AddField("Bot Version", BotVariables.Version, true);
+        embed.AddField("Bot Uptime", uptime.Humanize(), true);
+        embed.AddField("Discord Servers", serverList.Count, true);
+        embed.AddField("Discord Users", userList.Count, true);
+        embed.AddField("Streams", _twitchStreamDb.TwitchStreams.ToList().Count, true);
+        embed.AddField("Registered Users", _userDb.Users.ToList().Count, true);
+        embed.AddField("Manifest Version", manifest.Version, true);
+
+        await Context.Message.ReplyAsync(embed: embed.Build());
     }
 }
