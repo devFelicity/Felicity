a7f77de023a507d306732518d26bd2d974e3a9f6
Willow <will09600@gmail.com>
2022-07-14 14:43:58 +0200

    Re-add bot join/leave event logging

--- Felicity/Services/Hosted/DiscordStartupService.cs
+++ Felicity/Services/Hosted/DiscordStartupService.cs
@@ -53,6 +53,9 @@ public class DiscordStartupService : BackgroundService
         _discordShardedClient.MessageReceived += OnMessageReceived;
         _discordShardedClient.MessageUpdated += OnMessageUpdated;
 
+        _discordShardedClient.JoinedGuild += OnJoinedGuild;
+        _discordShardedClient.LeftGuild += OnLeftGuild;
+
         _discordShardedClient.InteractionCreated += OnInteractionCreated;
         _interactionService.SlashCommandExecuted += OnSlashCommandExecuted;
 
@@ -78,6 +81,53 @@ public class DiscordStartupService : BackgroundService
         {
             await _interactionService.RegisterCommandsGloballyAsync();
         }
+    }
+
+    private static async Task OnJoinedGuild(SocketGuild arg)
+    {
+        await arg.DownloadUsersAsync();
+
+        var embed = Embeds.MakeBuilder();
+        embed.Author = new EmbedAuthorBuilder
+        {
+            Name = "Felicity was added to a server."
+        };
+        embed.Title = arg.Name;
+        embed.Fields = new List<EmbedFieldBuilder>
+        {
+            new()
+            {
+                Name = "Owner",
+                Value = arg.Owner,
+                IsInline = true
+            },
+            new()
+            {
+                Name = "Members",
+                Value = arg.MemberCount,
+                IsInline = true
+            }
+        };
+
+        if (arg.IconUrl != null)
+            embed.ThumbnailUrl = arg.IconUrl;
+
+        if (arg.Description != null)
+            embed.Description = arg.Description;
+
+        await BotVariables.DiscordLogChannel!.SendMessageAsync(embed: embed.Build());
+    }
+
+    private static async Task OnLeftGuild(SocketGuild arg)
+    {
+        var embed = Embeds.MakeBuilder();
+        embed.Author = new EmbedAuthorBuilder
+        {
+            Name = "Felicity was removed from a server."
+        };
+        embed.Title = arg.Name;
+
+        await BotVariables.DiscordLogChannel!.SendMessageAsync(embed: embed.Build());
     }
 
     private static Task OnMessageUpdated(Cacheable<IMessage, ulong> arg1, SocketMessage arg2, ISocketMessageChannel arg3)
