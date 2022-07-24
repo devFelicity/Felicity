378e3fc6afba05829f5a215cb63efebe2f357a3d
Willow <will09600@gmail.com>
2022-07-24 20:21:49 +0200

    [6.5.1]

--- Felicity/Util/EmoteHelper.cs
+++ Felicity/Util/EmoteHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Security.Cryptography;
+using System.Text.Json;
 using Discord;
 using Discord.WebSocket;
 using DotNetBungieAPI.Models.Destiny;
@@ -85,36 +86,51 @@ internal static class EmoteHelper
         return result;
     }
 
-    public static string GetWeaponType(DestinyInventoryItemDefinition manifestItem)
+    public static string GetItemType(DestinyInventoryItemDefinition manifestItem)
     {
         var result = manifestItem.ItemSubType switch
         {
-            DestinyItemSubType.AutoRifle => "<:autorifle:996495566521520208> ",
-            DestinyItemSubType.Shotgun => "<:shotgun:996495567825948672> ",
-            DestinyItemSubType.Machinegun => "<:machinegun:996495568887087196> ",
-            DestinyItemSubType.HandCannon => "<:handcannon:996492277373476906> ",
-            DestinyItemSubType.RocketLauncher => "<:rocketlauncher:996493601083244695> ",
-            DestinyItemSubType.FusionRifle => "<:fusionrifle:996495565082873976> ",
-            DestinyItemSubType.SniperRifle => "<:sniperrifle:996492271212040243> ",
-            DestinyItemSubType.PulseRifle => "<:pulserifle:996493599871078491> ",
-            DestinyItemSubType.ScoutRifle => "<:scoutrifle:996492274953371769> ",
-            DestinyItemSubType.Sidearm => "<:sidearm:996492272411619470> ",
-            DestinyItemSubType.Sword => "<:sword:996492273795727361> ",
-            DestinyItemSubType.FusionRifleLine => "<:linearfusion:996497905865195540> ",
-            DestinyItemSubType.GrenadeLauncher => "<:grenadelauncher:996492276228436000> ",
-            DestinyItemSubType.SubmachineGun => "<:submachinegun:996493598495359096> ",
-            DestinyItemSubType.TraceRifle => "<:tracerifle:996495569650466929> ",
-            DestinyItemSubType.Bow => "<:bow:996493602354114640> ",
-            DestinyItemSubType.Glaive => "<:glaive:996495571126845491> ",
             DestinyItemSubType.None => manifestItem.ItemType switch
             {
-                DestinyItemType.Vehicle => "<:sparrow:996727310805893181> ",
-                DestinyItemType.Ship => "<:ship:996727309069471815> ",
+                DestinyItemType.Vehicle => "<:SW:996727310805893181> ",
+                DestinyItemType.Ship => "<:SP:996727309069471815> ",
                 _ => string.Empty
             },
+            _ => GetItemType(manifestItem.ItemSubType)
+        };
+
+        return result != string.Empty ? result : "<:CS:996724235634491523> ";
+    }
+
+    public static string GetItemType(DestinyItemSubType? weaponDestinyItemType)
+    {
+        var result = weaponDestinyItemType switch
+        {
+            DestinyItemSubType.AutoRifle => "<:AR:996495566521520208> ",
+            DestinyItemSubType.Shotgun => "<:SG:996495567825948672> ",
+            DestinyItemSubType.Machinegun => "<:LMG:996495568887087196> ",
+            DestinyItemSubType.HandCannon => "<:HC:996492277373476906> ",
+            DestinyItemSubType.RocketLauncher => "<:RL:996493601083244695> ",
+            DestinyItemSubType.FusionRifle => "<:FR:996495565082873976> ",
+            DestinyItemSubType.SniperRifle => "<:SR:996492271212040243> ",
+            DestinyItemSubType.PulseRifle => "<:PR:996493599871078491> ",
+            DestinyItemSubType.ScoutRifle => "<:ScR:996492274953371769> ",
+            DestinyItemSubType.Sidearm => "<:SA:996492272411619470> ",
+            DestinyItemSubType.Sword => "<:SRD:996492273795727361> ",
+            DestinyItemSubType.FusionRifleLine => "<:LFR:996497905865195540> ",
+            DestinyItemSubType.GrenadeLauncher => "<:GL:996492276228436000> ",
+            DestinyItemSubType.SubmachineGun => "<:SMG:996493598495359096> ",
+            DestinyItemSubType.TraceRifle => "<:TR:996495569650466929> ",
+            DestinyItemSubType.Bow => "<:BW:996493602354114640> ",
+            DestinyItemSubType.Glaive => "<:GV:996495571126845491> ",
+            DestinyItemSubType.HelmetArmor => "<:helmet:996490149728899122> ",
+            DestinyItemSubType.GauntletsArmor => "<:gloves:996490148025995385> ",
+            DestinyItemSubType.ChestArmor => "<:chest:996490146922901655> ",
+            DestinyItemSubType.LegArmor => "<:boots:996490145224200292> ",
+            DestinyItemSubType.ClassArmor => "<:class:996490144066572288> ",
             _ => string.Empty
         };
 
-        return result != string.Empty ? result : "<:consumables:996724235634491523> ";
+        return result != string.Empty ? result : "<:CS:996724235634491523> ";
     }
 }
