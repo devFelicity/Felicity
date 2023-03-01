3f7d473a844641c14c4bb29f768855aa7f886f62
Willow <will09600@gmail.com>
2023-03-01 18:45:06 +0100

    Remove `/vendor mods`

--- Felicity/DiscordCommands/Interactions/VendorCommands.cs
+++ Felicity/DiscordCommands/Interactions/VendorCommands.cs
@@ -1,4 +1,4 @@
-﻿using Discord;
+using Discord;
 using Discord.Interactions;
 using DotNetBungieAPI.Extensions;
 using DotNetBungieAPI.HashReferences;
@@ -89,7 +89,7 @@ public class VendorCommands : InteractionModuleBase<ShardedInteractionContext>
         }
     }
 
-    [SlashCommand("mods", "Get list of mods currently available at vendors.")]
+    /*[SlashCommand("mods", "Get list of mods currently available at vendors.")]
     public async Task Mods()
     {
         if (!await BungieApiUtils.CheckApi(_bungieClient))
@@ -110,7 +110,7 @@ public class VendorCommands : InteractionModuleBase<ShardedInteractionContext>
         var modCache = await ProcessModData.FetchInventory(_bungieClient, lg, user);
 
         await FollowupAsync(embed: await ProcessModData.BuildEmbed(_bungieClient, modCache, user));
-    }
+    }*/
 
     [SlashCommand("saint14", "Fetch Saint-14 (Trials of Osiris) reputation rewards for the week.")]
     public async Task Saint14()
